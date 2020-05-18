f92cbd102e28e02b61ca3fe1ed1db80ee3252683
Gabyfle <gaby.santamaria@outlook.fr>
2020-05-18 16:35:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Asuro library
+A tiny C library to program the ATMega8L chipped on ASURO.
+
+## Usage
+### Initialization
+
+Every program have to initialize its state by using the `init` function provided in `asurolib.h`.
+
+### Playing with LEDs
+
+ASURO has a total of 5 LEDs. Here are available LEDs on ASURO:
+
+* Status (GREEN and RED)
+* Front (RED)
+* 
+
+## Credits
+* A lot of ideas have been taken from: [Asuro](http://www.arexx.com/downloads/asuro/asuro_src.zip) by Jan Grewe
---
+++ src/asurolib.h
@@ -0,0 +1,43 @@
+/**
+ * Asuro development library
+ * @author Gabriel Santamaria <gaby.santamaria@outlook.fr>
+ * 18/05/2020
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef ASURO_ASUROLIB_H
+#define ASURO_ASUROLIB_H
+
+/** Asuro library constants **/
+
+/* Boolean values */
+#define  false    0
+#define  true    1
+
+/* State values */
+#define  OFF    0
+#define  ON     1
+
+/** Asuro library functions **/
+
+/*
+ * Initialization function
+ * This has to be called before any program startup
+ */
+void init(void);
+
+
+#ifdef ASUROLIB_LEDS
+/** Asuro LED functions **/
+#include "led.h"
+#endif
+#endif //ASURO_ASUROLIB_H
